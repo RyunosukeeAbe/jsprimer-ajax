0fe29a5fc60e6cb64ea76b4fd74a12e64720c864
RyunosukeeAbe <abe@ediplex.com>
2021-09-16 14:45:21 +0900

    finish

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
 function fetchUserInfo(userId) {
     fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
         .then(response => {
+            console.log(response.status);
             if (!response.ok) {
                 console.error("エラーレスポンス", response);
             } else {
                 return response.json().then(userInfo => {
-                    // HTMLの組み立て
-                    const view = escapeHTML`
-                    <h4>${userInfo.name} (@${userInfo.login})</h4>
-                    <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
-                    <dl>
-                        <dt>Location</dt>
-                        <dd>${userInfo.location}</dd>
-                        <dt>Repositories</dt>
-                        <dd>${userInfo.public_repos}</dd>
-                    </dl>
-                    `;
-                    // HTMLの挿入
-                    const result = document.getElementById("result");
-                    result.innerHTML = view;
+                    console.log(userInfo);
+
+                    // const view = escapeHTML`
+                    // <h4>${userInfo.name} (@${userInfo.login})</h4>
+                    // <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+                    // <dl>
+                    //     <dt>Location</dt>
+                    //     <dd>${userInfo.location}</dd>
+                    //     <dt>Repositories</dt>
+                    //     <dd>${userInfo.public_repos}</dd>
+                    // </dl>
+                    // `;
+                    // // HTMLの挿入
+                    // const result = document.getElementById("result");
+                    // result.innerHTML = view;
                 });
             }
         }).catch(error => {
             console.error(error);
         });
 }
+
+const view = `
+<h4>${userInfo.name} (@${userInfo.login})</h4>
+<img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+<dl>
+    <dt>Location</dt>
+    <dd>${userInfo.location}</dd>
+    <dt>Repositories</dt>
+    <dd>${userInfo.public_repos}</dd>
+</dl>
+`;
+
+const result = document.getElementById("result");
+result.innerHTML = view;
 
 function escapeSpecialChars(str) {
     return str
